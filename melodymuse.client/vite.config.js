78accd316eb756bfce4e0f11bd5c2c74304e9c5d
saikewei <58879129+saikewei@users.noreply.github.com>
2024-07-09 00:07:39 +0800

    重新创建项目

--- melodymuse.client/vite.config.js
+++ melodymuse.client/vite.config.js
@@ -47,7 +47,7 @@ export default defineConfig({
     server: {
         proxy: {
             '^/weatherforecast': {
-                target: 'https://localhost:7269/',
+                target: 'https://localhost:7223/',
                 secure: false
             }
         },
