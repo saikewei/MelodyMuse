53561ae01d591faebf7ecbcd1c3f77957cbb5706
AsapRobin <1476925796@qq.com>
2024-07-09 17:56:37 +0800

    增删改基本操作

--- MelodyMuse.Server/DatabaseOperation.cs
+++ MelodyMuse.Server/DatabaseOperation.cs
@@ -12,7 +12,7 @@ namespace MelodyMuse.Server
                 // 打开数据库连接
                 connection.Open();
                 Console.WriteLine("Connected to Oracle Database");
-                
+
                 // 创建一个OracleCommand对象，表示要在数据库上执行的SQL命令
                 using (OracleCommand command = new OracleCommand(sql, connection))
                 {
@@ -44,6 +44,77 @@ namespace MelodyMuse.Server
 
             // 返回读取到的结果字典列表
             return result;
+        }
+
+        public static int Insert(string sql, Dictionary<string, object> parameters)
+        {
+            // 使用指定的连接字符串创建并打开Oracle数据库连接
+            using (OracleConnection connection = new OracleConnection(ServerConnectionConstants.connectionString))
+            {
+                connection.Open();  // 打开数据库连接
+                Console.WriteLine("Connected to Oracle Database");  // 输出连接成功信息
+
+                // 创建一个OracleCommand对象，表示要在数据库上执行的SQL命令
+                using (OracleCommand command = new OracleCommand(sql, connection))
+                {
+                    // 遍历参数字典，将每个参数添加到OracleCommand对象中
+                    foreach (var param in parameters)
+                    {
+                        command.Parameters.Add(new OracleParameter(param.Key, param.Value));
+                    }
+
+                    // 执行插入操作，返回受影响的行数
+                    return command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // 更新操作
+        public static int Update(string sql, Dictionary<string, object> parameters)
+        {
+            // 使用指定的连接字符串创建并打开Oracle数据库连接
+            using (OracleConnection connection = new OracleConnection(ServerConnectionConstants.connectionString))
+            {
+                connection.Open();  // 打开数据库连接
+                Console.WriteLine("Connected to Oracle Database");  // 输出连接成功信息
+
+                // 创建一个OracleCommand对象，表示要在数据库上执行的SQL命令
+                using (OracleCommand command = new OracleCommand(sql, connection))
+                {
+                    // 遍历参数字典，将每个参数添加到OracleCommand对象中
+                    foreach (var param in parameters)
+                    {
+                        command.Parameters.Add(new OracleParameter(param.Key, param.Value));
+                    }
+
+                    // 执行更新操作，返回受影响的行数
+                    return command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // 删除操作
+        public static int Delete(string sql, Dictionary<string, object> parameters)
+        {
+            // 使用指定的连接字符串创建并打开Oracle数据库连接
+            using (OracleConnection connection = new OracleConnection(ServerConnectionConstants.connectionString))
+            {
+                connection.Open();  // 打开数据库连接
+                Console.WriteLine("Connected to Oracle Database");  // 输出连接成功信息
+
+                // 创建一个OracleCommand对象，表示要在数据库上执行的SQL命令
+                using (OracleCommand command = new OracleCommand(sql, connection))
+                {
+                    // 遍历参数字典，将每个参数添加到OracleCommand对象中
+                    foreach (var param in parameters)
+                    {
+                        command.Parameters.Add(new OracleParameter(param.Key, param.Value));
+                    }
+
+                    // 执行删除操作，返回受影响的行数
+                    return command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
