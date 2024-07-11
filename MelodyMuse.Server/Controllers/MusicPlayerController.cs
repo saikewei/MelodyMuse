931c8fc1749e30c9811ff8f465ee66b86ea01a1a
saikewei <58879129+saikewei@users.noreply.github.com>
2024-07-11 17:22:08 +0800

    添加了播放器api的get请求，用于请求音乐文件
    另外将gitignore也加入项目文件管理

---
+++ MelodyMuse.Server/Controllers/MusicPlayerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MelodyMuse.Server.Controllers
+{
+    [ApiController]
+    [Route("api/player")]
+    public class MusicPlayerController : Controller
+    {
+        private readonly string _musicFilePath = @"./Resources/";
+
+        // 通过歌曲ID获得歌曲的请求响应
+        [HttpGet]
+        [Route("{musicId}")]
+        public IActionResult StreamMusic(string musicId)
+        {
+            try
+            {
+                var fileStream = System.IO.File.OpenRead(_musicFilePath + musicId + ".mp3");
+                var contentType = "audio/mpeg";
+
+                return File(fileStream, contentType);
+            }
+            catch (Exception ex) when (
+                ex is FileNotFoundException ||
+                ex is UnauthorizedAccessException ||
+                ex is IOException)
+            {
+                // 文件未找到或无法访问时的处理逻辑
+                Console.WriteLine($"文件打开失败：{ex.Message}");
+                return NotFound(); // 返回 HTTP 404 响应
+            }
+        }
+    }
+}
