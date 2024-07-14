8530a1d4c3c748871a24ea4c82436c7132f1e216
Henry Kang <149673273+wukdhxbsdkjxn@users.noreply.github.com>
2024-07-14 20:40:01 +0800

    界面更改
    界面更改

--- melodymuse.client/src/components/login.vue
+++ melodymuse.client/src/components/login.vue
@@ -1,32 +1,84 @@
 <template>
   <div class="container">
-          <div class="login-wrapper">
-              <div class="header">MelodyMuse</div>
-              <div class="form-wrapper">
-                  <input type="tel" name="phonenumber" placeholder="手机号码" class="input-item">
-                  <input type="password" name="password" placeholder="密码" class="input-item">
-                  <div class="btn2">Login</div>
-              </div>
-          </div>
+    <div class="login-wrapper">
+      <div class="header">MelodyMuse</div>
+      <div class="form-wrapper">
+        <input type="tel" name="phonenumber" placeholder="手机号码" class="input-item" v-model="username">
+        <input type="password" name="password" placeholder="密码" class="input-item" v-model="password">
+        <div class="btn2" @click="login">Login</div>
       </div>
+      <p v-if="loginError" class="error-message">{{ loginError }}</p>
+    </div>
+  </div>
 </template>
 
+
+
 <script>
+
   export default {
+    data() {
+    return {
+      username: '',
+      password: '',
+      loginError: ''
+    };
+  },
+  methods: {
+    async login() {
+        // 验证手机号码是否为空
+      if (this.username.trim() === '') {
+        this.loginError = '手机号码不能为空。';
+        return;
+      }
+
+      // 验证密码是否为空
+      if (this.password.trim() === '') {
+        this.loginError = '密码不能为空。';
+        return;
+      }
+
+      // 验证手机号码长度和格式
+      if (!/^\d{11}$/.test(this.username)) {
+        this.loginError = '请输入11位有效的手机号码。';
+        return;
+      }
+
+      try {
+        const response = await axios.post('http://127.0.0.1:4523/m1/4804827-4459167-default/api/account/login?apifoxResponseId=487488274', {
+          msg: this.username,
+          token: this.password
+        });
+
+        if (response.status === 200 && response.data.token) {
+          alert('Login successful!');
+          // Store the token in localStorage or Vuex store
+          localStorage.setItem('token', response.data.token);
+          // Redirect to the dashboard or another page
+          this.$router.push('/dashboard');
+        } else {
+          this.loginError = response.data.msg || 'Login failed, please try again.';
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          this.loginError = error.response.data.msg || 'Login failed, please try again.';
+        } else {
+          console.error(error);
+          this.loginError = 'An error occurred during login. Please try again later.';
+        }
+      }
+    }
+  },
+
       name:"Login"
   }
+  
 </script>
-
 <style scoped>
-
-html {
-  height: 100%;
-}
-body {
+html, body {
   height: 100%;
 }
 .container {
-  /* margin-top: 5%; */
   height: 980px;
   width: 100%;
   background-image: linear-gradient(to right, #e1c1e4, white);
@@ -45,7 +97,7 @@ body {
 }
 .header {
   font-size: 36px;
-  color:#bb8bbe;
+  color: #bb8bbe;
   font-weight: bold;
   text-align: center;
   line-height: 200px;
@@ -54,7 +106,6 @@ body {
   display: block;
   width: 100%;
   margin-bottom: 25px;
-  border: 0;
   padding: 12px;
   border: 1px solid rgb(128, 125, 125);
   border-radius: 10px;
@@ -67,33 +118,24 @@ body {
 .btn2 {
   text-align: center;
   padding: 8px;
-  margin: 0 auto;
   width: 100%;
   margin-top: 40px;
   background-color: #c99fcb;
   color: #fff;
-  border-radius:10px;
-  display:inline-block;
-  cursor:pointer;
-  font-family:Arial;
-  font-size:20px;
-  text-decoration:none;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 20px;
 }
 .btn2:hover {
-    background-color:#bb8bbe;
+  background-color: #bb8bbe;
 }
 .btn2:active {
-    position:relative;
-    top:1px;
+  position: relative;
+  top: 1px;
 }
-
-.msg {
+.error-message {
+  color: red;
   text-align: center;
-  line-height: 88px;
+  margin-top: 10px;
 }
-a {
-  text-decoration-line: none;
-  color:#bb8bbe;
-}
-
 </style>
--- melodymuse.client/src/components/register.vue
+++ melodymuse.client/src/components/register.vue
@@ -1,32 +1,79 @@
 <template>
   <div class="container">
-          <div class="login-wrapper">
-              <div class="header">MelodyMuse</div>
-              <div class="form-wrapper">
-                  <input type="tel" name="phonenumber" placeholder="手机号码" class="input-item">               
-                  <input type="password" name="password" placeholder="密码" class="input-item">
-                  <input type="password" name="repassword" placeholder="再次确认密码" class="input-item">
-                  <div class="btn2">Register</div>
-              </div>
-          </div>
+    <div class="login-wrapper">
+      <div class="header">MelodyMuse</div>
+      <div class="form-wrapper">
+        <input type="tel" name="phonenumber" placeholder="手机号码" class="input-item" v-model="username">               
+        <input type="password" name="password" placeholder="密码" class="input-item" v-model="password">
+        <input type="password" name="repassword" placeholder="再次确认密码" class="input-item" v-model="confirmPassword">
+        <div class="btn2" @click="register">Register</div>
+        <p v-if="registerError" class="error-message">{{ registerError }}</p>
       </div>
+    </div>
+  </div>
 </template>
-  
+
 <script>
-  export default {
-      name:"Reg"
-  }
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      confirmPassword: '',
+      registerError: ''
+    };
+  },
+  methods: {
+    async register() {
+      // 验证手机号码长度和格式
+      if (!/^\d{11}$/.test(this.username)) {
+        this.registerError = '请输入11位有效的手机号码。';
+        return;
+      }
+
+      // 验证密码是否为空
+      if (this.password.trim() === '') {
+        this.registerError = '密码不能为空。';
+        return;
+      }
+
+      // 验证两次输入的密码是否一致
+      if (this.password !== this.confirmPassword) {
+        this.registerError = '两次输入的密码不一致。';
+        return;
+      }
+
+      try {
+        const response = await axios.post ('http://127.0.0.1:4523/m2/4804827-4459167-default/192699877', {
+          msg: this.username,
+
+
+          token: this.password
+        });
+
+        if (response.status === 200) {
+          alert('Registration successful!');
+          // Redirect to the login page
+          this.$router.push('/login');
+        } else {
+          this.registerError = response.data.msg || 'Registration failed, please try again.';
+        }
+      } catch (error) {
+        console.error(error);
+        this.registerError = 'An error occurred during registration. Please try again later.';
+      }
+    }
+  },
+  name: "Reg"
+}
 </script>
 
 <style scoped>
-html {
-  height: 100%;
-}
-body {
+html, body {
   height: 100%;
 }
 .container {
-  /* margin-top: 5%; */
   height: 980px;
   width: 100%;
   background-image: linear-gradient(to right, #e1c1e4, white);
@@ -45,12 +92,11 @@ body {
 }
 .header {
   font-size: 36px;
-  color:#bb8bbe;
+  color: #bb8bbe;
   font-weight: bold;
   text-align: center;
   line-height: 200px;
 }
-
 .input-item {
   display: block;
   width: 100%;
@@ -62,30 +108,31 @@ body {
   font-size: 15px;
   outline: none;
 }
-.input-item:placeholder {
+.input-item::placeholder {
   text-transform: uppercase;
 }
 .btn2 {
   text-align: center;
   padding: 8px;
-  margin: 0 auto;
   width: 100%;
   margin-top: 25px;
   background-color: #c99fcb;
   color: #fff;
-  border-radius:10px;
-  display:inline-block;
-  cursor:pointer;
-  font-family:Arial;
-  font-size:20px;
-  text-decoration:none;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 20px;
 }
 .btn2:hover {
-    background-color:#bb8bbe;
+  background-color: #bb8bbe;
 }
 .btn2:active {
-    position:relative;
-    top:1px;
+  position: relative;
+  top: 1px;
+}
+.error-message {
+  color: red;
+  text-align: center;
+  margin-top: 10px;
 }
 .msg {
   text-align: center;
@@ -93,7 +140,6 @@ body {
 }
 a {
   text-decoration-line: none;
-  color:#bb8bbe;
+  color: #bb8bbe;
 }
-
 </style>
