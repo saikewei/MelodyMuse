303c0aecc43e55258598b7343c8b4e65a7fa2d5a
AsapRobin <1476925796@qq.com>
2024-07-14 18:36:52 +0800

    音乐播放器

---
+++ vue-music/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <MusicPlayer />
+  </div>
+</template>
+
+<script>
+import MusicPlayer from './components/MusicPlayer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MusicPlayer
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
