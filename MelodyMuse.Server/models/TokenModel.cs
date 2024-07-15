9e7a304b1a8e1e23c514c4537ecd7b1f1ec4b687
CoArray <3061632379@qq.com>
2024-07-15 20:10:49 +0800

    TokenModel一些注释信息，昨天忘了提交

--- MelodyMuse.Server/models/TokenModel.cs
+++ MelodyMuse.Server/models/TokenModel.cs
@@ -1,5 +1,11 @@
-﻿namespace MelodyMuse.Server.models
+﻿/*
+    JWT生成与解析存放信息的模型
+ */
+
+
+namespace MelodyMuse.Server.models
 {
+    //Token解析可获得UserId,Username,UserPhone
     public class ParsedTokenModel
     {
         public string UserID { get; set; }
@@ -9,6 +15,9 @@
     }
 
 
+
+    //生成JWT需要UserId,UserName,UserPhone    
+    //JWT只会在login时生成，其他页面只需要解析即可
     public class GenerateTokenModel
     {
         public string UserID { get; set; }
