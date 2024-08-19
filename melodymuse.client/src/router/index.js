79d90691e8370f0466c237c16bb1ca9991b2489a
happyDecember <152608184+happyDecember@users.noreply.github.com>
2024-08-19 13:38:13 +0800

    跳转到具体歌手页面

--- melodymuse.client/src/router/index.js
+++ melodymuse.client/src/router/index.js
@@ -75,7 +75,7 @@ const routes = [
         component: Register
     },
     {
-        path: '/singerDetail',
+        path: '/SingerDetail/:artistId',
         name: 'SingerDetail',
         component: SingerDetail
     },
